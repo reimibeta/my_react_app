64443e3183187474fe2bb17c31442c6b788670aa
reimibeta <65539218+reimibeta@users.noreply.github.com>
2021-11-11 22:44:52 +0700

    Initial commit

---
+++ src/webpages/home.js
@@ -0,0 +1,11 @@
+//file: src/webpages/home.js
+import React from 'react';
+const Home = () => {
+    return (
+        <div>
+            <h1>Book App</h1>
+            <p>This is home page</p>
+        </div>
+    );
+};
+export default Home;
---
+++ src/webpages/index.js
@@ -0,0 +1,54 @@
+//file: src/webpages/index.js
+import React from 'react';
+import {
+//   BrowserRouter as Router,
+  Switch,
+  NavLink,
+  BrowserRouter,
+  Route,
+  Routes,
+//   Link
+} from "react-router-dom";
+import Home from './home';
+import MyBooks from './mybooks';
+import Favorites from './favorites';
+const Webpages = () => {
+    return(
+        <div>
+            <BrowserRouter>
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink 
+                                exact to="/" 
+                                activeClassName="selected">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink 
+                                to="/mybooks" 
+                                activeClassName="selected">
+                                My Books
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink 
+                                to="/favorites" 
+                                activeClassName="selected">
+                                Favorites
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+        
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path = "/mybooks" element = {<MyBooks />} />
+                    <Route path = "/favorites" element = {<Favorites />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
+export default Webpages;
