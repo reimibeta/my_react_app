573fb72215de4bf273d46c526e4added8dd5f909
reimi <65539218+reimibeta@users.noreply.github.com>
2021-11-14 10:44:05 +0700

    .

--- src/webpages/index.js
+++ src/webpages/index.js
@@ -7,6 +7,8 @@ import {
   Routes,
 //   Link
 } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 import Home from './home';
 import MyBooks from './mybooks';
 import Favorites from './favorites';
@@ -14,7 +16,17 @@ const Webpages = () => {
     return(
         <div>
             <BrowserRouter>
-                <nav>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand as={NavLink} to="/">Navbar</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                            <Nav.Link as={NavLink} to="/mybooks">My Books</Nav.Link>
+                            <Nav.Link as={NavLink} to="/favorites">Favorites</Nav.Link>
+                        </Nav>
+                </Container>
+            </Navbar>
+                {/* <nav>
                     <ul>
                         <li>
                             <NavLink 
@@ -38,7 +50,7 @@ const Webpages = () => {
                             </NavLink>
                         </li>
                     </ul>
-                </nav>
+                </nav> */}
         
                 <Routes>
                     <Route exact path="/" element={<Home />} />
