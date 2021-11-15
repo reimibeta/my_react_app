781520bff1e958f52be2a0ff0fc1465fdc7c3969
reimi <65539218+reimibeta@users.noreply.github.com>
2021-11-15 10:56:15 +0700

    .

--- src/webpages/home.js
+++ src/webpages/home.js
@@ -1,11 +1,32 @@
 //file: src/webpages/home.js
 import React from 'react';
+import { Container } from 'react-bootstrap';
 const Home = () => {
+    const numbers = [1, 2, 3];
+    const index = numbers.indexOf(2);
+    // const adds = [numbers.slice(0, index)];
+    // adding
+    const added = [...numbers.slice(0, index), 4 , ...numbers.slice(index)];
+    console.log('add: ', added);
+    // removing
+    const removed = numbers.filter(n => n !== 2);
+    console.log('remove', removed);
+    // updating
+    const updated = numbers.map(n => n === 2 ? 20 : n);
+    console.log('update', updated);
+    // fetch
+    numbers.map(n => {
+        console.log('fetch: ', n);
+    });
     return (
-        <div>
+        <Container>
             <h1>Book App</h1>
-            <p>This is home page</p>
-        </div>
+            {
+                numbers.map(n => {
+                    return <p>{n}</p>
+                })
+            }
+        </Container>
     );
 };
 export default Home;
--- src/webpages/mybooks.js
+++ src/webpages/mybooks.js
@@ -1,14 +1,30 @@
 //file: src/webpages/mybooks.js
 import React, { useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import { Map } from 'immutable';
+
 const MyBooks = () => {
     useEffect(() => {
         document.title = 'My Books';
     });
+    //
+    let book = Map({ title: 'Naruto'});
+    function publish(book){
+        // book.isPunlished = true;
+        return book.set('isPublished', true);
+    }
+    // test
+    console.log('book before: ', book);
+    // console.log('get book: ', book.get('title'))
+    console.log('get book before (js): ', book.toJS())
+    book = publish(book);
+    console.log('book after: ', book);
+    console.log('get book after (js): ', book.toJS())
     return (
-        <div>
+        <Container>
             <h1>Book App</h1>
             <p>These are my books...</p>
-        </div>
+        </Container>
     );
 };
 export default MyBooks;
